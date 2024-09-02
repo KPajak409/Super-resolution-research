2143cd61cbb5579bacd5ee5684d862c434168420
KPajak409 <pajak.karol414@gmail.com>
2024-09-03 00:29:33 +0200

    Corrected cnn model architectures

--- conv_tests.py
+++ conv_tests.py
@@ -1,18 +1,18 @@
-#%%
-
+# %%
 import torch
 import torch.nn as nn
 
-class Conv_simple(nn.Module):
-    def __init__(self, upscale=2):
+
+class Conv_upsamplex4(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, (1,1))
-        self.conv2 = nn.Conv2d(16, 32, (3,3), padding=1)
-        self.conv3 = nn.Conv2d(32, 64, (3,3), padding=1)
-        self.conv4 = nn.Conv2d(64, 64, (3,3), padding=1)
-        self.px_shuffle = nn.PixelShuffle(upscale)
+        self.conv1 = nn.Conv2d(3, 16, (1, 1))
+        self.conv2 = nn.Conv2d(16, 32, (3, 3), padding=1)
+        self.conv3 = nn.Conv2d(32, 48, (3, 3), padding=1)
+        self.conv4 = nn.Conv2d(48, 48, (3, 3), padding=1)
+        self.px_shuffle = nn.PixelShuffle(4)  # C_out = C_in / upscale_factor**2
         self.act = nn.ReLU()
-        
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.act(x)
@@ -22,15 +22,38 @@ class Conv_simple(nn.Module):
         x = self.act(x)
         x = self.conv4(x)
         x = self.px_shuffle(x)
-        
+
         return x
-    
- 
-    
+
+
+class Conv_upsamplex2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, (1, 1))
+        self.conv2 = nn.Conv2d(16, 32, (3, 3), padding=1)
+        self.conv3 = nn.Conv2d(32, 12, (3, 3), padding=1)
+        self.px_shuffle = nn.PixelShuffle(2)  # C_out = C_in / upscale_factor**2
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.act(x)
+        x = self.conv2(x)
+        x = self.act(x)
+        x = self.conv3(x)
+        x = self.px_shuffle(x)
+
+        return x
+
+
 if __name__ == "__main__":
-    model = Conv_simple(upscale=4)
+    modelx2 = Conv_upsamplex2()
+    modelx4 = Conv_upsamplex4()
     x = torch.rand([1, 3, 56, 56])
-    y = model(x)
-    print(x.shape, y.shape)
-    
+    yx2 = modelx2(x)
+    yx4 = modelx4(x)
+    print(f"Input shape: \t\t{x.shape}")
+    print(f"Conv_upsamplex2 out: \t{yx2.shape}")
+    print(f"Conv_upsamplex4 out: \t{yx4.shape}")
+
 # %%

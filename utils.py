Fix patch dim scaling, additional display func

utils.py
@@ -53,6 +53,32 @@ def unpatchify(patches, unfold_shape, target_shape) -> torch.Tensor:
     return p
 
 
+def p2img_forward(image, target, scale, patch_size, stride, model):
+    # turn image into patches
+    overlap = patch_size - stride
+    p_lr, un_lr = patchify(image, patch_size=patch_size, stride=stride, overlap=overlap)
+
+    # modify unfold dimensions to fit with upscaled image
+    un_hr = [
+        (un_lr[i] - overlap) * scale if i > 4 else un_lr[i] for i in range(len(un_lr))
+    ]
+
+    # forward batch of patches with overlap
+    with torch.no_grad():
+        output = model(p_lr)
+
+    # discard of overlap
+    output = output[
+        :,
+        :,
+        (overlap // 2) * scale : -(overlap // 2) * scale,
+        (overlap // 2) * scale : -(overlap // 2) * scale,
+    ]
+
+    # convert patches to single image with the same size as orginal
+    return unpatchify(output, un_hr, target.shape)
+
+
 def plot_patches(tensor, n_patches):
     fig = plt.figure(figsize=(8, 8))
     grid = ImageGrid(
@@ -67,35 +93,12 @@ def plot_patches(tensor, n_patches):
     plt.show()
 
 
-def p2img_forward(image, target, scale, patch_size, stride, model):
-    # turn image into patches
-    overlap = patch_size - stride
-    p_lr, un_lr = patchify(image, patch_size=patch_size, stride=stride, overlap=overlap)
-
-    # modify unfold dimensions to fit with upscaled image
-    un_hr = [
-        un_lr[i] * scale - overlap * scale if i > 4 else un_lr[i]
-        for i in range(len(un_lr))
-    ]
-
-    # forward batch of patches with overlap
-    with torch.no_grad():
-        output = model(p_lr)
-
-    # discard of overlap
-    output = output[:, :, overlap:-overlap, overlap:-overlap]
-
-    # convert patches to single image with the same size as orginal
-    return unpatchify(output, un_hr, target.shape)
-
-
 def plot_out_target(output, target):
     output_permute = torch.permute(output, (0, 2, 3, 1))
     target_permute = torch.permute(target, (0, 2, 3, 1))
 
     psnr_value = round(psnr(output, target).item(), 3)
     ssim_value = round(ssim(output, target).item(), 3)
-    print(output_permute.shape, target_permute.shape)
     # pair = torch.hstack((output_permute[0], target_permute[0]))
     plt.subplot(1, 2, 1)
     plt.imshow(output_permute[0])
@@ -106,5 +109,33 @@ def plot_out_target(output, target):
     plt.imshow(target_permute[0])
     plt.axis("off")
     plt.title("Ground Truth")
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.show()
+
+
+def plot_src_out_target(src, output, target):
+    src_permute = torch.permute(src, (0, 2, 3, 1))
+    output_permute = torch.permute(output, (0, 2, 3, 1))
+    target_permute = torch.permute(target, (0, 2, 3, 1))
+
+    psnr_value = round(psnr(output, target).item(), 3)
+    ssim_value = round(ssim(output, target).item(), 3)
+    # pair = torch.hstack((output_permute[0], target_permute[0]))
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(src_permute[0])
+    plt.axis("off")
+    plt.title("LR", fontsize=9)
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(output_permute[0])
+    plt.axis("off")
+    plt.title(f"Psnr: {psnr_value}, SSIM: {ssim_value}", fontsize=9)
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(target_permute[0])
+    plt.axis("off")
+    plt.title("Ground Truth", fontsize=9)
     plt.subplots_adjust(wspace=0, hspace=0)
     plt.show()